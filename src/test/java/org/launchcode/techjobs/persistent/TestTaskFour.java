2c47378e1164b019037a9c5a7db3b78f4b8a4057
UGcode101 <142420124+UGcode101@users.noreply.github.com>
2024-05-30 22:04:06 -0500

    task 2 complete

--- src/test/java/org/launchcode/techjobs/persistent/TestTaskFour.java
+++ src/test/java/org/launchcode/techjobs/persistent/TestTaskFour.java
@@ -1,24 +1,13 @@
 package org.launchcode.techjobs.persistent;
 
 import jakarta.persistence.ManyToMany;
-import mockit.Expectations;
-import mockit.Mocked;
 import org.junit.jupiter.api.Test;
-import org.launchcode.techjobs.persistent.controllers.HomeController;
-import org.launchcode.techjobs.persistent.controllers.ListController;
 import org.launchcode.techjobs.persistent.models.Employer;
-import org.launchcode.techjobs.persistent.models.Job;
 import org.launchcode.techjobs.persistent.models.Skill;
-import org.launchcode.techjobs.persistent.models.data.JobRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.ui.ExtendedModelMap;
-import org.springframework.ui.Model;
-import org.springframework.validation.Errors;
 
 import java.io.IOException;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.*;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
